e2f9f338dfe565e68f0232905ab7e196fd08d2d4
alexandrejuk <ale_santos.soares@hotmail.com>
2017-10-09 20:09:33 -0300

    chore: create eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from './app';
 
 app.listen(app.get('port'), () => {
-    console.log(`Servido no ar ${app.get('port')}`);
-})
+  console.log(`Servido no ar ${app.get('port')}`);
+});
--- test/integration/app.js
+++ test/integration/app.js
@@ -1,96 +1,90 @@
 describe('Routes Books', () => {
-const Books = app.datasource.models.Books,
-defaultBook = {
+  const Books = app.datasource.models.Books;
+  const defaultBook = {
     id: 1,
-    name: 'Default Book'
-};
-    beforeEach(done => {
-        Books
-        .destroy({where: {}})
-        .then(() => Books.create(defaultBook))
-        .then(() => {
-            done();
+    name: 'Default Book',
+  };
+  beforeEach((done) => {
+    Books
+      .destroy({ where: {} })
+      .then(() => Books.create(defaultBook))
+      .then(() => {
+        done();
+      });
+  });
+
+  describe('Route GET /books', () => {
+    it('should return a list of books', (done) => {
+      request
+        .get('/books')
+        .end((err, res) => {
+          expect(res.body[0].name).to.be.eql(defaultBook.name);
+          expect(res.body[0].id).to.be.eql(defaultBook.id);
+
+          done(err);
         });
     });
-    
-    describe('Route GET /books', () => {
-        it('should return a list of books', done => {
+  });
 
-            request
-                .get('/books')
-                .end((err, res) => {
+  describe('Route GET /books/{id}', () => {
+    it('should a list of book', (done) => {
+      request
+        .get(`/books/${defaultBook.id}`)
+        .end((err, res) => {
+          expect(res.body.name).to.be.eql(defaultBook.name);
+          expect(res.body.id).to.be.eql(defaultBook.id);
 
-                    expect(res.body[0].name).to.be.eql(defaultBook.name);
-                    expect(res.body[0].id).to.be.eql(defaultBook.id);
-
-                    done(err);
-                });
+          done(err);
         });
     });
+  });
 
-    describe('Route GET /books/{id}', () => {
-        it('should a list of book', done => {
+  describe('Route POST /books', () => {
+    it('should create a book', (done) => {
+      const newBook = {
+        id: 2,
+        name: 'New Book',
+      };
 
-            request
-                .get(`/books/${defaultBook.id}`)
-                .end((err, res) => {
+      request
+        .post('/books')
+        .send(newBook)
+        .end((err, res) => {
+          expect(res.body.name).to.be.eql(newBook.name);
+          expect(res.body.id).to.be.eql(newBook.id);
 
-                    expect(res.body.name).to.be.eql(defaultBook.name);
-                    expect(res.body.id).to.be.eql(defaultBook.id);
-
-                    done(err);
-                });
+          done(err);
         });
     });
+  });
 
-    describe('Route POST /books', () => {
-        it('should create a book', done => {
-            const newBook = {
-                id: 2,
-                name: 'New Book'
-            };
+  describe('Route PUT /books/{id}', () => {
+    it('should update a book', (done) => {
+      const updateBook = {
+        id: 2,
+        name: 'Update Book',
+      };
 
-            request
-                .post('/books')
-                .send(newBook)
-                .end((err, res) => {
+      request
+        .put(`/books/${updateBook.id}`)
+        .send(updateBook)
+        .end((err, res) => {
+          expect(res.body).to.be.eql([0]);
 
-                    expect(res.body.name).to.be.eql(newBook.name);
-                    expect(res.body.id).to.be.eql(newBook.id);
-
-                    done(err);
-                });
+          done(err);
         });
     });
+  });
 
-    describe('Route PUT /books/{id}', () => {
-        it('should update a book', done => {
-            const updateBook = {
-                id: 2,
-                name: 'Update Book'
-            };
+  describe('Route DELETE /books/{id}', () => {
+    it('should delete a book', (done) => {
+      request
+        .delete(`/books/${defaultBook.id}`)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.eql(204);
 
-            request
-                .put(`/books/${updateBook.id}`)
-                .send(updateBook)
-                .end((err, res) => {
-                    expect(res.body).to.be.eql([0]);
-
-                    done(err);
-                });
+          done(err);
         });
     });
-
-    describe('Route DELETE /books/{id}', () => {
-        it('should delete a book', done => {
-        
-            request
-                .delete(`/books/${defaultBook.id}`)
-                .end((err, res) => {
-                    expect(res.statusCode).to.be.eql(204);
-
-                    done(err);
-                });
-        });
-    });
+  });
 });
