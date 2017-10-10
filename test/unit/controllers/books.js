ee6cdc13e900ea45cd4aa9fad35336ea43ce6fae
alexandrejuk <ale_santos.soares@hotmail.com>
2017-10-09 22:03:35 -0300

    chore: create test unit in controllers

---
+++ test/unit/controllers/books.js
@@ -0,0 +1,110 @@
+import BooksController from '../../../controllers/books';
+
+describe('Controllers: Books', () => {
+  describe('Get all books: getAll()', () => {
+    it('should return a list of books', () => {
+      const Books = {
+        findAll: td.function(),
+      };
+
+      const expectedResponse = [{
+        id: 1,
+        name: 'Test Book',
+        created_at: '2017-10-09T23:31:18.182Z',
+        updated_at: '2017-10-09T23:31:18.182Z',
+      }];
+
+      td.when(Books.findAll({})).thenResolve(expectedResponse);
+
+      const booksController = new BooksController(Books);
+      return booksController.getAll()
+        .then(response => expect(response.data).to.be.eql(expectedResponse));
+    });
+  });
+
+
+  describe('Get a book: getById()', () => {
+    it('should return a books', () => {
+      const Books = {
+        findOne: td.function(),
+      };
+
+      const expectedResponse = [{
+        id: 1,
+        name: 'Test Book',
+        created_at: '2017-10-09T23:31:18.182Z',
+        updated_at: '2017-10-09T23:31:18.182Z',
+      }];
+
+      td.when(Books.findOne({ where: { id: 1 } })).thenResolve(expectedResponse);
+
+      const booksController = new BooksController(Books);
+      return booksController.getById({ id: 1 })
+        .then(response => expect(response.data).to.be.eql(expectedResponse));
+    });
+  });
+
+
+  describe('Create a book: create()', () => {
+    it('should create a book', () => {
+      const Books = {
+        create: td.function(),
+      };
+      const requestBody = {
+        name: 'Test Book',
+      };
+      const expectedResponse = [{
+        id: 1,
+        name: 'Test Book',
+        created_at: '2017-10-09T23:31:18.182Z',
+        updated_at: '2017-10-09T23:31:18.182Z',
+      }];
+
+      td.when(Books.create(requestBody)).thenResolve(expectedResponse);
+
+      const booksController = new BooksController(Books);
+      return booksController.create(requestBody)
+        .then((response) => {
+          expect(response.statusCode).to.be.eql(201);
+          expect(response.data).to.be.eql(expectedResponse);
+        });
+    });
+  });
+
+  describe('Update a book: create()', () => {
+    it('should update a book', () => {
+      const Books = {
+        update: td.function(),
+      };
+      const requestBody = {
+        name: 'Test Book',
+      };
+      const expectedResponse = [{
+        id: 1,
+        name: 'Test Book Update',
+        created_at: '2017-10-09T23:31:18.182Z',
+        updated_at: '2017-10-09T23:31:18.182Z',
+      }];
+
+      td.when(Books.update(requestBody, { where: { id: 1 } })).thenResolve(expectedResponse);
+
+      const booksController = new BooksController(Books);
+      return booksController.update(requestBody, { id: 1 })
+        .then(response => expect(response.data).to.be.eql(expectedResponse));
+    });
+  });
+
+  describe('Delete a book: create()', () => {
+    it('should delete a book', () => {
+      const Books = {
+        destroy: td.function(),
+      };
+
+      td.when(Books.destroy({ where: { id: 1 } })).thenResolve({});
+
+      const booksController = new BooksController(Books);
+      return booksController.delete({ id: 1 })
+        .then(response => expect(response.statusCode).to.be.eql(204));
+    });
+  });
+});
